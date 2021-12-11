bf2f93cae13886ee8ed676178bed1df0f1d5fd6e
Shineglow <61727978+Shineglow@users.noreply.github.com>
2021-12-11 17:27:54 +0500

    Fixing bugs in move_checker
    Move_checker now correctly find paths from checker.

--- game/logic/move_checker.gd
+++ game/logic/move_checker.gd
@@ -2,27 +2,42 @@ extends Object
 
 class_name Path_finder, "res://logic/move_checker.gd"
 
+
 var _field
 var _field_size
 
-var _paths = []
+var _jump_cells = []
+var _move_cells = []
+var cell
 
 # я пришёл к решению хранить сопостовимые данны в двух массивах, 
 # для простоты итерации и чтобы избежать большого количества тернарных блоков
 var _arround = [Vector2(-1, -1),Vector2(0, -1), Vector2(1, -1), Vector2(-1, 0),Vector2(1, 0),Vector2(-1, 1),Vector2(0, 1),Vector2(1, 1)]
-var _arrow_name = ["move_upleft","move_up","move_upright","move_left","move_right","move_downleft","move_down","move_downright","jump"]
+var _arrow_name = ["move_upleft","move_up","move_upright","move_left","move_right","move_downleft","move_down","move_downright","move_jump"]
 
 func set_new(field: Field):
-	_field = field.field
+	_field = field
 	_field_size = field.field.size()
 
-func find_moves_arround_checker(checker_pos: Vector2):
+func clear_old():
+	for i in _field.field:
+		for y in i:
+			(y as Cell).highlight(false)
+
+func find_moves_from_checker(checker_pos: Vector2):
+	clear_old()
 	
-	var a = _try_jump(checker_pos, [])
+	if cell == checker_pos:
+		cell = null
+		return
+	cell = checker_pos
+	var _jump_cells = _try_jump(checker_pos, [])
+	var _move_cells = _get_empty_cells_arround(checker_pos)
 	
-	for i in a:
-		print(i)
-	pass
+	for i in _jump_cells:
+		(_field.field[i.y][i.x] as Cell).highlight(true)
+	for i in _move_cells:
+		(_field.field[i.y][i.x] as Cell).highlight(true)
 
 func _get_empty_cells_arround(current: Vector2):
 	var result_array = []
@@ -32,9 +47,10 @@ func _get_empty_cells_arround(current: Vector2):
 		var condition_a = cell_pos.x >= 0 and cell_pos.x < _field_size
 		var condition_b = cell_pos.y >= 0 and cell_pos.y < _field_size
 		if condition_a and condition_b:
-			var current_cell = (_field[cell_pos.x][cell_pos.y] as Cell)
-			if current_cell.checker_on_cell == null:
-				result_array.append(current_cell)
+			var current_cell = (_field.field[cell_pos.y][cell_pos.x] as Cell)
+			if !current_cell.is_checker_contain:
+				current_cell.change_move_arrow(_arrow_name[i])
+				result_array.append(cell_pos)
 	
 	return result_array
 
@@ -47,23 +63,28 @@ func _try_jump(current: Vector2, last_path):
 		# если координаты в рамках поля
 		if condition_a and condition_b:
 			# переменная проверяемой ячейки
-			var current_cell = (_field[cell_pos.x][cell_pos.y] as Cell)
+			var current_cell = (_field.field[cell_pos.y][cell_pos.x] as Cell)
 			if current_cell.is_checker_contain:
 				var next_pos = current + _arround[i]*2
 				condition_a = next_pos.x >= 0 and next_pos.x < _field_size
 				condition_b = next_pos.y >= 0 and next_pos.y < _field_size
+				# если координаты клетки для прыжка в рамках поля
 				if condition_a and condition_b:
 					
-					var next_cell = (_field[next_pos.x][next_pos.y] as Cell)
+					var next_cell = (_field.field[next_pos.y][next_pos.x] as Cell)
 					if !next_cell.is_checker_contain:
 						# если маршрут больше нуля
 						if last_path.size() > 0:
 							if last_path.find(next_pos) == -1:
+								next_cell.change_move_arrow(_arrow_name[8])
 								last_path.append(next_pos)
 								
+								# если маршрут к следующей ячейке уже есть
 								var path_size = next_cell.path.size()
 								if path_size > 0:
+									# и если этот маршрут длиннее текущего
 									if path_size > last_path.size():
+										# заменяем путь к клетке на текущий
 										next_cell.path = last_path
 								else:
 									next_cell.path = last_path
@@ -73,6 +94,7 @@ func _try_jump(current: Vector2, last_path):
 										last_path.append(y)
 						else:
 							last_path.append(next_pos)
+							next_cell.change_move_arrow(_arrow_name[8])
 								
 							var path_size = next_cell.path.size()
 							if path_size > 0:
