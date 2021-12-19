b25b60d9ac179999f9c1aabd4c4fd6fc67cee6a5
Shineglow <61727978+Shineglow@users.noreply.github.com>
2021-12-19 20:19:21 +0500

    Fix checkers animate
    Animations work with tween interpolation. Animations playing from another class.

--- game/logic/field.gd
+++ game/logic/field.gd
@@ -1,5 +1,32 @@
 extends Control
 
+class custom_animator:
+	var _target_cell: Cell
+	var _moving_checker: Checker
+	var _tween: Tween
+	var _path = []
+	
+	func _init(cell: Cell, checker: Checker, tween: Tween, path):
+		_target_cell = cell
+		_moving_checker = checker
+		_tween = tween
+		_path = path
+	
+	func animate_move_checker():
+		_path.invert()
+		for i in _path:
+			var end = i*_target_cell.rect_min_size
+			var start = _moving_checker.rect_position
+			var time = _get_lenght(start-end)*0.005
+			_tween.interpolate_property(_moving_checker, "rect_position", null, end, 0.5, Tween.TRANS_QUAD)
+			_tween.start()
+			yield(_tween, "tween_completed")
+		_moving_checker.position = _target_cell.position
+	
+	func _get_lenght(vec: Vector2):
+		return sqrt(vec.x*vec.x + vec.y*vec.y)
+
+
 class_name Field, "res://logic/field.gd"
 
 var CELL_TEMPLATE = preload("res://Interface/Cell.tscn")
@@ -15,36 +42,9 @@ var checkers = []
 var active_checker: Checker
 var is_already_move: bool
 onready var ghost_checker = $ghost_checker
+var gc_position
 
 onready var _tween = $Tween
-
-func animate_move_checker(cell: Cell):
-	_tween.remove_all()
-	var path = []
-	cell.position
-	path.append_array(_get_path(cell.position))
-	path.invert()
-	path.remove(0)
-	for i in path:
-		var end = i*cell.rect_min_size
-		var start = active_checker.rect_position
-		_tween.interpolate_property(active_checker, "rect_position", null, i*cell.rect_min_size, 1, Tween.TRANS_QUAD)
-		_tween.start()
-		print(yield(_tween, "tween_completed"))
-		
-
-func foo(start, end):
-	_tween.interpolate_property(active_checker, "rect_position", null, end, (start-end).length()*0.1, Tween.TRANS_QUAD)
-
-func _get_path(previews_cell_pos: Vector2):
-	var sub_path = []
-	var cell = (field[previews_cell_pos.y][previews_cell_pos.x] as Cell)
-	sub_path.append(cell.previews_cell_pos)
-	
-	if cell.path_lenght <= 2:
-		return sub_path
-
-	return sub_path.append_array(_get_path(cell.previews_cell_pos))
 
 signal on_checker_click(checker)
 signal try_move()
@@ -63,19 +63,11 @@ func cell_click_react(cell: Cell):
 		ghost_checker.texture = active_checker.get_node("checker_texture").texture
 		ghost_checker.rect_position = cell.rect_position
 		ghost_checker.visible = true
-		
-		#var pos = active_checker.position
-		#(field[pos.y][pos.x] as Cell).is_checker_contain = false
-		#active_checker.move_checker(cell.position)
-		#cell.checker_on_cell = active_checker
-		#cell.is_checker_contain = true
+		gc_position = cell.position
 		is_already_move = true
 	elif cell.checker_on_cell == active_checker:
 		is_already_move = false
 		ghost_checker.visible = false
-		# перемещение шашки на её прежнее место
-		# снять выделение с шашки
-		pass
 
 func checker_pressed(checker):
 	emit_signal("on_checker_click",checker)
@@ -89,7 +81,6 @@ func select_checker_logic(checker):
 		else:
 			active_checker = null
 			checker.is_selected = false
-			return
 	else:
 		active_checker = checker
 		active_checker.is_selected = true
@@ -97,17 +88,30 @@ func select_checker_logic(checker):
 func end_turn():
 	var pos = active_checker.position
 	(field[pos.y][pos.x] as Cell).is_checker_contain = false
-	var target_cell_pos = ghost_checker.rect_position/ghost_checker.rect_min_size
-	var cell = field[target_cell_pos.y][target_cell_pos.x] as Cell
+	var cell = field[gc_position.y][gc_position.x] as Cell
 	
 	cell.checker_on_cell = active_checker
 	cell.is_checker_contain = true
 	
-	animate_move_checker(cell)
+	var animator = custom_animator.new(cell, active_checker, _tween, _get_path(cell.position))
+	animator.animate_move_checker()
 	
 	is_already_move = false
 	active_checker.is_selected = false
 	active_checker = null
+
+func _get_path(current_pos: Vector2):
+		var sub_path = []
+		var _target_cell = (field[current_pos.y][current_pos.x] as Cell)
+		sub_path.append(_target_cell.position)
+	
+		if _target_cell.path_lenght == 0:
+			return sub_path
+		
+		for i in _get_path(_target_cell.previews_cell_pos):
+			sub_path.append(i)
+		
+		return sub_path
 
 func generate_field(field_size: int):
 	if field_size >= 7:
@@ -162,7 +166,9 @@ func castling(checker_pos_1: Vector2, checker_pos_2: Vector2):
 func reset():
 	if active_checker != null:
 		active_checker.is_selected = false
+		active_checker = null
 	is_already_move = false
+	ghost_checker.visible = false
 	for y in field:
 		for x in y:
 			(x as Cell).reset()
